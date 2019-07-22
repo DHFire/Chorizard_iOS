cfefab0ed1d5becf14fc68413948b92986f5494e
Hayden Hoki <hayden.hoki@homie.com>
2019-07-22 09:18:20 -0600

    Table view can send data to detail view.

---
+++ Chorizard/To-Do/ToDoTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoTableViewCell.swift
+//  Chorizard
+//
+//  Created by Hayden Hoki on 7/19/19.
+//  Copyright © 2019 Hayden Hoki. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+}
